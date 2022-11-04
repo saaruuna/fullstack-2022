f43983a73cffe778236028a51677e34c2f402ca8
Uuna Saarela <uuna.saarela@gmail.com>
2022-11-04 10:01:27 +0200

    part 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,61 @@
+const Course = ({ course }) => {
+    return (
+        <>
+          <Header course={course} />
+          <Content parts={course.parts} />
+        </>
+      )
+}
+
+const Header = ({ course }) => {
+    return (
+      <div>
+        <h1> {course.name} </h1>
+      </div>
+    )
+}
+
+const Part = ({ part }) => {
+    return (
+        <li>{part.name} {part.exercises}</li>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+    <div>
+        <ul>
+            {parts.map(part => <Part key={part.id} part={part} />)}
+        </ul>
+    </div>
+    )
+}
+
+const App = () => {
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return <Course course={course} />
+  }
+  
+
+  export default App
